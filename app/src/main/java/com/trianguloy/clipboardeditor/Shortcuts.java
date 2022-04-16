75f5509876e6ef98980fb34e34ac2d15adf20df6
TrianguloY <correo--correo@hotmail.com>
2022-04-16 12:13:19 +0200

    shortcut to clear the clipboard
    Closes #3

---
+++ app/src/main/java/com/trianguloy/clipboardeditor/Shortcuts.java
@@ -0,0 +1,33 @@
+package com.trianguloy.clipboardeditor;
+
+import android.app.Activity;
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.widget.Toast;
+
+/**
+ * Activity that will clear the clipboard when launched, then exit
+ */
+public class Shortcuts extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // get
+        ClipboardManager clipboard = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
+
+        // clear
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            // the easy way, just call 'clear'
+            clipboard.clearPrimaryClip();
+        } else {
+            // the not-so-easy way, manually set as empty
+            clipboard.setPrimaryClip(ClipData.newPlainText("", ""));
+        }
+
+        Toast.makeText(this, R.string.toast_cleared, Toast.LENGTH_SHORT).show();
+        finish();
+    }
+}
